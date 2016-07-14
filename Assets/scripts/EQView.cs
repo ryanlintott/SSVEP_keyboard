69d88adf0ccab0652cb951e0bd7eb9dca03a669a
Ryan Lintott <lintott@gmail.com>
2016-07-14 18:26:52 +0100

    Added Spectrum Analyzer using microphone input.  Audio stream seems delayed by several seconds and creates a feedback loop.  Doesn't seem to work without playing the audio.

---
+++ Assets/scripts/EQView.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EQView : MonoBehaviour {
+
+	private Slider[] eqBars;
+	private float max = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		eqBars = GetComponentsInChildren<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void UpdateEQ (float[] values) {
+		for (int i = 0; i < eqBars.Length; i++) {
+			float tempValue = values[(i*values.Length)/eqBars.Length];
+			if (tempValue > max) {
+				max = tempValue;
+			}
+			eqBars[i].value = (tempValue/max);
+		}
+	}
+}
---
+++ Assets/scripts/MicrophoneInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+//using UnityEngine.Microphone;
+
+public class MicrophoneInput : MonoBehaviour {
+
+	public AudioSource audio;
+	private float[] samples;
+	public EQView _eqView;
+
+	private int numSamples = 1024;
+	// Use this for initialization
+	void Start () {
+		audio.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+		audio.loop = true;
+		samples = new float[numSamples];
+		//while (!Microphone.GetPosition(null)){}
+		audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		audio.GetSpectrumData (samples, 0, FFTWindow.Hamming);
+		_eqView.UpdateEQ(samples);
+
+		Debug.Log(samples[512].ToString());
+	}
+}
