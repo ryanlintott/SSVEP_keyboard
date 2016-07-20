d49dfbe2da38e5b04a06bc715f4a578b90fe1269
Ryan Lintott <lintott@gmail.com>
2016-07-20 17:43:01 +0100

    Changed some audio files and added test tone audio files.  Added Hz based method for selecting a range.  Made the keys transparent when not active.  Added use of outputfrequency so that 1000Hz frequency will work on 44100Hz audio files.

--- Assets/scripts/EQView.cs
+++ Assets/scripts/EQView.cs
@@ -6,7 +6,9 @@ public class EQView : MonoBehaviour {
 
 	private Slider[] eqBars;
 	private float max = 0.0f;
-	private int groupValues;
+	private float groupValues;
+	private int startValue;
+	private int endValue;
 
 	// Use this for initialization
 	void Start () {
@@ -18,18 +20,40 @@ public class EQView : MonoBehaviour {
 	
 	}
 
-	public void UpdateEQ (float[] values) {
-		groupValues = values.Length/eqBars.Length;
-		Debug.Log(groupValues.ToString());
+	private void UpdateEQ (float[] values) {
+		Debug.Log("Length: "+values.Length.ToString()+" Start: "+startValue.ToString()+" End: "+endValue.ToString()+"groupValues: "+groupValues.ToString());
 		for (int i = 0; i < eqBars.Length; i++) {
 			float tempValue = 0.0f;
+			int iGroups = Mathf.RoundToInt(i * groupValues);
 			for (int j = 0; j < groupValues; j++) {
-				tempValue += values[i*groupValues+j];
+				tempValue += values[startValue + iGroups + j];
 			}
+			if (groupValues >= 2.0f) {
+				tempValue /=  Mathf.FloorToInt(groupValues);
+			}
+			//boost values to make them more visible
+			//tempValue = Mathf.Sqrt(tempValue);
+
 			if (tempValue > max) {
 				max = tempValue;
+				//Debug.Log("Max: "+max.ToString());
 			}
 			eqBars[i].value = (tempValue/max);
 		}
 	}
+
+	public void UpdateEQClamped (float[] values, float start, float end) {
+		startValue = Mathf.RoundToInt(start*values.Length);
+		endValue = Mathf.RoundToInt(end*values.Length);	
+		groupValues = (endValue-startValue)*1.0f/eqBars.Length;
+		UpdateEQ(values);
+	}
+
+	public void UpdateEQHzRange (float[] values, int fTarget, int fWidth, float fMax) {
+		startValue = Mathf.FloorToInt((fTarget - (fWidth / 2)) * values.Length / fMax);
+		endValue = Mathf.FloorToInt((fTarget + (fWidth / 2)) * values.Length / fMax);
+		groupValues = (endValue-startValue)*1.0f/eqBars.Length;
+		UpdateEQ(values);
+	}
+
 }
--- Assets/scripts/MicrophoneInput.cs
+++ Assets/scripts/MicrophoneInput.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 //using UnityEngine.Microphone;
 
@@ -6,28 +6,71 @@ public class MicrophoneInput : MonoBehaviour {
 
 	public AudioSource audio;
 	private float[] samples;
+	private float[] lastSamples;
 	public EQView _eqView;
 	public bool useMicrophone = true;
-
-	private int numSamples = 1024;
+	public bool useClamp = true;
+	public float leftClamp = 0.0f;
+	public float rightClamp = 1.0f;
+	public float smoothing = 0.0f;
+	public int inputHz = 40000;
+	public int fTarget = 1000;
+	public int fWidth = 120;
+	private int numSamples = 8192;
+	private float fMax;
 	// Use this for initialization
 	void Start () {
 		audio.loop = true;
 		samples = new float[numSamples];
+		lastSamples = new float[numSamples];
+		fMax = AudioSettings.outputSampleRate;
+		//fMax = inputHz;
+		Debug.Log(fMax.ToString());
 		if (useMicrophone) {
-			audio.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+			audio.clip = Microphone.Start("Built-in Microphone", true, 10, inputHz);
 			while (Microphone.GetPosition(null) <= 0){}
 		}
+		//audio.mute = true;
 		audio.Play();
+		audio.GetSpectrumData (lastSamples, 0, FFTWindow.Hanning);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		audio.GetSpectrumData (samples, 0, FFTWindow.Hamming);
-		//audio.GetOutputData (samples, 0, FFTWindow.Hamming);
-		_eqView.UpdateEQ(samples);
+		System.Array.Copy(samples,lastSamples,numSamples);
+		audio.GetSpectrumData (samples, 0, FFTWindow.Hanning);
+		//audio.GetOutputData (samples, 0);
+		for (int i = 0; i < numSamples; i++) {
+			samples[i] = smoothing * lastSamples[i] + (1/smoothing) * samples[i];
+		}
 
+		// int xScale = 8;
+		// int yScale = 400;
+		// int xoffset = 3000;
+		// int yoffset = 1000;
+		// for (int i = 1; i < numSamples-1; i++) {
+		//  Debug.DrawLine (new Vector3 ((i - 1)*xScale-xoffset, samples[i]*yScale-yoffset, 0),
+		// 				new Vector3 (i*xScale-xoffset, samples[i + 1]*yScale-yoffset, 0), Color.red);
+
+		// 	Debug.DrawLine (new Vector3 ((i - 1)*xScale-xoffset, Mathf.Log(samples[i])*yScale-yoffset, 0),
+		// 				new Vector3 (i*xScale-xoffset, Mathf.Log(samples[i + 1])*yScale-yoffset, 0), Color.cyan);
+
+		// 	Debug.DrawLine (new Vector3 (Mathf.Log(i - 1)*xScale-xoffset, samples[i]*yScale-yoffset, 0),
+		// 				new Vector3 (Mathf.Log(i)*xScale-xoffset, samples[i + 1]*yScale-yoffset, 0), Color.green);
+
+		// 	Debug.DrawLine (new Vector3 (Mathf.Log(i - 1)*xScale-xoffset, Mathf.Log(samples[i])*yScale-yoffset, 0),
+		// 				new Vector3 (Mathf.Log(i)*xScale-xoffset, Mathf.Log(samples[i + 1])*yScale-yoffset, 0), Color.yellow);
+		//  }
+
+
+		if (useClamp) {
+			//Used for updating eq with % clamps on either side
+			_eqView.UpdateEQClamped(samples, leftClamp, rightClamp);
+		} else {
+			//Used for updating eq with a target range specified in Hz
+			_eqView.UpdateEQHzRange(samples, fTarget, fWidth, fMax/2);
+		}
+		
 		//Debug.Log(samples[512].ToString());
 	}
 }
