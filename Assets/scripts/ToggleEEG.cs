ae13ae7d16270791949742198840c4dd8689455f
Ryan Lintott <lintott@gmail.com>
2016-07-21 16:14:50 +0100

    Added 1kHz tone output.

--- Assets/scripts/ToggleEEG.cs
+++ Assets/scripts/ToggleEEG.cs
@@ -8,8 +8,10 @@ public class ToggleEEG : MonoBehaviour {
 	private GridLayoutGroup eeggrid;
 	private RectTransform eegrect;
 	private float topSmall;
+	private float bottomSmall;
 	private float eqHeightSmall;
 	public float topLarge = 0.0f;
+	public float bottomLarge = 0.0f;
 	public float eqHeightLarge = 700.0f;
 	private bool sizeLarge = false;
 
@@ -19,6 +21,7 @@ public class ToggleEEG : MonoBehaviour {
 		eeggrid = GetComponentInChildren<GridLayoutGroup>();
 		eegrect = GetComponent<RectTransform>();
 		topSmall = eegrect.GetTop().y;
+		bottomSmall = eegrect.GetBottom().y;
 		eqHeightSmall = eeggrid.cellSize.y;
 	}
 	
@@ -33,12 +36,14 @@ public class ToggleEEG : MonoBehaviour {
 			eeggrid.cellSize = new Vector2(eeggrid.cellSize.x, eqHeightSmall);
 			//eeggrid.cellSize.Set(eeggrid.cellSize.x, eqHeightSmall);
 			eegrect.SetTop(topSmall);
+			eegrect.SetBottom(bottomSmall);
 			sizeLarge = false;
 			Debug.Log("ToggleEEGSmall Cellsize: "+eeggrid.cellSize.y.ToString()+"eqHeight: "+eegrect.GetTop().y.ToString());
 		} else {
 			eeggrid.cellSize = new Vector2(eeggrid.cellSize.x, eqHeightLarge);
 			//eeggrid.cellSize.Set(eeggrid.cellSize.x, eqHeightLarge);
 			eegrect.SetTop(topLarge);
+			eegrect.SetBottom(bottomLarge);
 			sizeLarge = true;
 			Debug.Log("ToggleEEGLarge Cellsize: "+eeggrid.cellSize.y.ToString()+"eqHeight: "+eegrect.GetTop().y.ToString());
 		}
