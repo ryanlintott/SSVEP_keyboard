32900060dbb73d6ec71293eaf9263ec40be05d36
Ryan Lintott <lintott@gmail.com>
2016-11-30 11:27:34 +0000

    Added a procedural 1kHz tone so there is no longer need for the tone wav file.

--- Assets/scripts/LineOutput.cs
+++ Assets/scripts/LineOutput.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
 
 public class LineOutput : MonoBehaviour {
+// un-optimized version
+	public double frequency = 1000;
+	public double gain = 0.05;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	private double increment;
+	private double phase;
+	public double sampling_frequency = 48000;
+
+	void OnAudioFilterRead(float[] data, int channels) {
+		// update increment in case frequency has changed
+		increment = frequency * 2 * Math.PI / sampling_frequency;
+		for (var i = 0; i < data.Length; i = i + channels) {
+			phase = phase + increment;
+			// this is where we copy audio data to make them “available” to Unity
+			data[i] = (float)(gain*Math.Sin(phase));
+			// if we have stereo, we copy the mono data to each channel
+			if (channels == 2) data[i + 1] = data[i];
+			if (phase > 2 * Math.PI) phase = 0;
+		}
 	}
 }
+
