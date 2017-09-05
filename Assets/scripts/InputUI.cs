2b5e8fd20e9684b309a1a2bc34ced1b1c64189b1
Ryan Lintott <lintott@gmail.com>
2017-09-05 18:51:42 +0100

    Added toggle for lower section UI between buttons and diff display Removed Mode button (not really useful any more)

---
+++ Assets/scripts/InputUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputUI : MonoBehaviour {
+
+	public int activePanel = 0;
+	public GameObject[] _panels;
+
+	// Use this for initialization
+	void Start () {
+		ActivatePanel();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ActivatePanel () {
+		foreach (GameObject p in _panels) {
+			p.active = false;
+		}
+		_panels[activePanel].active = true;
+	}
+
+	public void NextPanel () {
+		activePanel = (activePanel + 1) % _panels.Length;
+		ActivatePanel();
+	}
+
+	public void TogglePanel (bool toggle) {
+		if (toggle) {
+			activePanel = 1;
+		} else {
+			activePanel = 0;
+		}
+		ActivatePanel();
+	}
+}
